95c72a78ddd0de0d82bfb61b7272f99fc1de4eb0
AbhishekMShastryK <abhishekshastry1999@gmail.com>
2021-05-10 01:22:47 +0530

    plot update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import {React} from 'react';
 import './App.css';
 
 
-const iframe1 = '<iframe width="400" height="250" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/3?height=250&width=400&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Temperature&type=line"></iframe>';
-const iframe2 = '<iframe width="400" height="250" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/2?height=250&width=400&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Humidity&type=line"></iframe>';
-const iframe3 = '<iframe width="400" height="250" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/4?height=250&width=400&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Pressure&type=line"></iframe>';
-const iframe4 = '<iframe width="400" height="250" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/5?height=250&width=400&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Soil+moisture&type=line"></iframe>';
+const iframe1 = '<iframe width="380" height="230" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/3?height=230&width=380&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Temperature&type=line"></iframe>';
+const iframe2 = '<iframe width="380" height="230" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/2?height=230&width=380&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Humidity&type=line"></iframe>';
+const iframe3 = '<iframe width="380" height="230" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/4?height=230&width=380&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Pressure&type=line"></iframe>';
+const iframe4 = '<iframe width="380" height="230" style="border: 3px solid #666666;" src="https://thingspeak.com/channels/1384648/charts/5?height=230&width=380&bgcolor=%23eeeeee&color=%23333333&dynamic=true&results=30&title=Soil+moisture&type=line"></iframe>';
 
 function Iframe(props) {
   return (<div dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />);
