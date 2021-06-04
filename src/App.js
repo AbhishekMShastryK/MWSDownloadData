a3ed8fd9e3fdbd1ab3721c08ea29bc4bec665004
AbhishekMShastryK <abhishekshastry1999@gmail.com>
2021-06-05 00:26:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import moment from "moment";
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import SaveIcon from '@material-ui/icons/Save';
+import Container from '@material-ui/core/Container';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -99,6 +100,7 @@ useEffect(() => {
   ];
   // data1 = data1.map(row => ({...row, created_at: moment(row.created_at).format("YYYY-MM-DD")}))
   return (
+    <Container maxWidth="sm">
     <div className="App">
       <h1>Micro Weather Station</h1>
       <p>The below links are the data from Micro Weather Station Node 1, Node 2 and aggregate of both the nodes.</p><br />
@@ -134,6 +136,7 @@ useEffect(() => {
       </div>
       
     </div>
+    </Container>
   );
 }
 
