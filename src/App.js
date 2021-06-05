ce723805b86744de55d71d06233bc3b032875ca0
AbhishekMShastryK <abhishekshastry1999@gmail.com>
2021-06-05 11:44:07 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import SaveIcon from '@material-ui/icons/Save';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 
 
 const useStyles = makeStyles((theme) => ({
